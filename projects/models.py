1448b4a0d1b8cb7c41baaa4cbfb50ba088f7df05
JuangarcDev <juangarciagrr@gmail.com>
2024-02-29 23:52:01 -0500

    Creacion Tablas, problemas uso de GDAL

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from django.contrib.gis.db import models as gis_models
-from django.contrib.gis.geos import Point
+#from osgeo import gdal
+#from osgeo import ogr
+#from osgeo import osr
+#from django.contrib.gis.db import models as gis_models
+#from django.contrib.gis.geos import Point
 from rest_framework import serializers
 
 # Create your models here.
@@ -15,22 +18,22 @@ class Project(models.Model):
     
     from rest_framework import serializers
 
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = ['id', 'titulo', 'calificacion', 'pais']
+#class ProjectSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = Project
+#        fields = ['id', 'titulo', 'calificacion', 'pais']
 
 #AHORA CON LOCALIZACION
-class MovieLocation(models.Model):
-    titulo = models.CharField(max_length=300)
-    calificacion = models.DecimalField(max_digits=5, decimal_places=2)
-    pais = models.CharField(max_length=100)
-    location = gis_models.PointField(geography=True, null=True, blank=True)
+#class MovieLocation(models.Model):
+#    titulo = models.CharField(max_length=300)
+#    calificacion = models.DecimalField(max_digits=5, decimal_places=2)
+#    pais = models.CharField(max_length=100)
+#    location = gis_models.PointField(geography=True, null=True, blank=True)
 
-    def __str__(self):
-        return self.titulo
+#    def __str__(self):
+#        return self.titulo
 
-class MovieLocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MovieLocation
-        fields = ['titulo', 'calificacion', 'pais', 'location']
+#class MovieLocationSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = MovieLocation
+#        fields = ['titulo', 'calificacion', 'pais', 'location']
